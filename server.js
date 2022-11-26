200fed6803a956c9e7aa3cb39729ebf92b8db1b7
Muhammad Jawad <54392202+M-Jawad-Malik@users.noreply.github.com>
2022-11-26 22:23:04 +0500

    new change

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const image=require('./Controllers/image')
 const db=require('knex')({
     client:'pg',
     connection:{
-         connectionString:process.env.DATABASE_URL,
-        ssl:true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 })
 // db.select().from('users').then(
