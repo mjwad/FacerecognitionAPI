b82e78562eedc390ba2610494a23a43f883eeb85
Muhammad Jawad <54392202+M-Jawad-Malik@users.noreply.github.com>
2022-11-26 21:45:58 +0500

    new change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image=require('./Controllers/image')
 const db=require('knex')({
     client:'pg',
     connection:{
-         connectionString:`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
+         connectionString:process.env.DATABASE_URL,
         ssl:true
     }
 })
