86f9cf6cc3f5eb747f65f9576d32caf4f4a4f8a5
Muhammad Jawad <54392202+M-Jawad-Malik@users.noreply.github.com>
2022-11-27 01:28:48 +0500

    'msg'

--- server.js
+++ server.js
@@ -22,8 +22,14 @@ const app=express()
 app.use(bodyParser.json())
 app.use(cors())
 app.get('/',(req,res)=>{
-
-    res.send('no repsonse')
+    const db=require('knex')({
+        client:'pg',
+        connection:{
+            connectionString:process.env.DATABASE_URL,
+            ssl:true
+        }
+    })
+    res.json(db)
 })
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
 app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
